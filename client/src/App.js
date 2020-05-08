c32266f1b8f2873814f1be1fedf73e9ba8669d85
Aldap <aldap@assupol.co.za>
2020-05-08 22:35:02 +0200

    Alert added (continue) Monday

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,42 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
 
+import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
-
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
+import AlertState from './context/alert/AlertState';
 
 import './App.css';
 
 const App = () => {
   return (
+
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar title=" Contact List" icon="fas fa-id-card-alt" />
-            <div className="container">
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} /> 
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar title=" Contact List" icon="fas fa-id-card-alt" />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
-    </AuthState> 
+    </AuthState>
   );
 }
 
